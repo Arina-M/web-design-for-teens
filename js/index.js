bb356450aea8bbc17c4299ddee76270bdce9bc00
Arina Miahkykh <arishka.mail@gmail.com>
2024-04-23 17:13:54 +0300

    show/hide password done

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
-$('.hide-password').click(function (){
-    $('#password-singIn').attr('type', 'text');
-    $(this).hide();
-    $('#show-password').show();
+//show/hide password
+$(document).ready(function() {
+    $('.hide-password').click(function() {
+        var passwordField = $(this).siblings('.password');
+        var passwordInputType = passwordField.attr('type');
+        if (passwordInputType === 'password') {
+            passwordField.attr('type', 'text');
+            $(this).attr('src', '../images/show-password.svg').attr('alt', 'Show password');
+        } else {
+            passwordField.attr('type', 'password');
+            $(this).attr('src', '../images/hide-password.svg').attr('alt', 'Hide password');
+        }
+    });
 });
 
-$('.show-password').click(function (){
-    $('#password-singIn').attr('type', 'password');
-    $(this).hide();
-    $('#hide-password').show();
-});
+
 
 
